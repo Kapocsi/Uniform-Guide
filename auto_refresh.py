63381a77e759672e03ea2b57501c0e44a179d443
thomaskapocsi <tomkap011@gmail.com>
2022-10-12 10:11:28 -0600

    v0

---
+++ auto_refresh.py
@@ -0,0 +1,26 @@
+import time
+import markdown
+
+def main(lines):
+    markdownText = markdown.markdown(lines, extensions=['tables','md_in_html']) 
+    with open("index.html", "w+") as f:
+        f.write(markdownText)
+
+
+
+
+
+if __name__ == "__main__":
+    lines = ""
+    prevline = ""
+    while 1:
+        lines = ""
+        with open("index.md") as f:
+            for line in f:
+                lines = lines + line
+        if lines != prevline:
+            main(lines)
+            prevline = lines
+            print("FileUpdate")
+        else:
+            time.sleep(1)
