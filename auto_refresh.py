44dd73ca69b2eafa7ea9c9cfbc2c81eab80d7650
Thomas Kapocsi <tomkap011@gmail.com>
2022-10-13 07:19:00 -0600

    reformated python code

--- auto_refresh.py
+++ auto_refresh.py
@@ -1,18 +1,15 @@
 import time
 import markdown
 
+
 def main(lines):
-    markdownText = markdown.markdown(lines, extensions=['tables','md_in_html']) 
+    markdownText = markdown.markdown(lines, extensions=["tables", "md_in_html"])
     with open("index.html", "w+") as f:
         f.write(markdownText)
 
 
-
-
-
 if __name__ == "__main__":
-    lines = ""
-    prevline = ""
+    lines,prevline = "",""
     while 1:
         lines = ""
         with open("index.md") as f:
